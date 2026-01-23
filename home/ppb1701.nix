2603d59ac188fe0c85000a72aa2ac208c7f4c6c7
ppb1701 <ppb1701@users.noreply.github.com>
2026-01-23 14:54:52 -0500

    pix alias

--- home/ppb1701.nix
+++ home/ppb1701.nix
@@ -173,7 +173,7 @@
 ║ CONFIG EDITING:                                            ║
 ║   ec         - Edit configuration.nix                      ║
 ║   eh         - Edit home/ppb1701.nix                       ║
-║   ebkup      - Edit modules/backups.nix                    ║
+║   ebu        - Edit modules/backups.nix                    ║
 ║   en         - Edit modules/networking.nix                 ║
 ║   es         - Edit modules/services.nix                   ║
 ║   esy        - Edit modules/system.nix                     ║
@@ -221,7 +221,7 @@
    # Config editing
    ec = "sudo micro /etc/nixos/configuration.nix";
    eh = "sudo micro /etc/nixos/home/ppb1701.nix";
-   ebkup = "sudo mivro /etc/nixos/modules/backups.nix";
+   ebu = "sudo micro /etc/nixos/modules/backups.nix";
    en = "sudo micro /etc/nixos/modules/networking.nix";
    em = "sudo micro /etc/nixos/modules/monitoring.nix";
    es = "sudo micro /etc/nixos/modules/services.nix";
